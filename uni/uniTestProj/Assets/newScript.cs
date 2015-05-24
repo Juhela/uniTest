c0fea834b484674a53c99bea56fb381036eb69c2
Jussi Latvala <juhela@utu.fi>
2015-05-24 22:49:03 +0300

    turhaa kamaa

---
+++ uni/uniTestProj/Assets/newScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class newScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("test");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
